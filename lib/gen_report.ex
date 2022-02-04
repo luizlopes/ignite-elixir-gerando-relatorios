74e22cdc4c5ce617baf6ea285e898ec5c695ffdb
Luiz Lopes <lg.oliveira.lopes@gmail.com>
2022-02-04 00:09:33 -0300

    Adds function to build report using parallel processing

--- lib/gen_report.ex
+++ lib/gen_report.ex
@@ -18,6 +18,15 @@ defmodule GenReport do
     |> Enum.reduce(@initial_report, &sum_values/2)
   end
 
+  def build_from_many(params) when not is_list(params),
+    do: {:error, "Insira uma lista de nomes de arquivos"}
+
+  def build_from_many(filenames) do
+    filenames
+    |> Task.async_stream(&build/1)
+    |> Enum.reduce(@initial_report, fn {:ok, result}, report -> sum_reports(report, result) end)
+  end
+
   defp sum_values([name, hours, _day, month, year], acc) do
     acc
     |> add_name(name)
@@ -57,4 +66,43 @@ defmodule GenReport do
 
   defp sum_hours(nil, value), do: value
   defp sum_hours(value_1, value_2), do: value_1 + value_2
+
+  defp sum_reports(
+         %{
+           "all_hours" => all_hours_1,
+           "hours_per_month" => hours_per_month_1,
+           "hours_per_year" => hours_per_year_1
+         },
+         %{
+           "all_hours" => all_hours_2,
+           "hours_per_month" => hours_per_month_2,
+           "hours_per_year" => hours_per_year_2
+         }
+       ) do
+    %{
+      "all_hours" => merge_all_hours(all_hours_1, all_hours_2),
+      "hours_per_month" => merge_hours_per_month(hours_per_month_1, hours_per_month_2),
+      "hours_per_year" => merge_hours_per_year(hours_per_year_1, hours_per_year_2)
+    }
+  end
+
+  defp merge_all_hours(all_hours_1, all_hours_2) do
+    merge_maps(all_hours_1, all_hours_2)
+  end
+
+  defp merge_hours_per_month(hours_per_month_1, hours_per_month_2) do
+    Map.merge(hours_per_month_1, hours_per_month_2, fn _key, value1, value2 ->
+      merge_maps(value1, value2)
+    end)
+  end
+
+  defp merge_hours_per_year(hours_per_year_1, hours_per_year_2) do
+    Map.merge(hours_per_year_1, hours_per_year_2, fn _key, value1, value2 ->
+      merge_maps(value1, value2)
+    end)
+  end
+
+  defp merge_maps(value1, value2) do
+    Map.merge(value1, value2, fn _key, v1, v2 -> v1 + v2 end)
+  end
 end
--- test/gen_report_test.exs
+++ test/gen_report_test.exs
@@ -5,6 +5,7 @@ defmodule GenReportTest do
   alias GenReport.Support.ReportFixture
 
   @file_name "gen_report.csv"
+  @filenames ["part_1.csv", "part_2.csv", "part_3.csv"]
 
   describe "build/1" do
     test "When passing file name return a report" do
@@ -17,6 +18,22 @@ defmodule GenReportTest do
       response = GenReport.build()
 
       assert response == {:error, "Insira o nome de um arquivo"}
+    end
+  end
+
+  describe "build_from_many/1" do
+    test "when a file list is provided, builds the report" do
+      response = GenReport.build_from_many(@filenames)
+
+      assert response == ReportFixture.build()
+    end
+
+    test "when a string is provided, returns error" do
+      filenames = "part_1.csv"
+
+      response = GenReport.build_from_many(filenames)
+
+      assert response == {:error, "Insira uma lista de nomes de arquivos"}
     end
   end
 end
